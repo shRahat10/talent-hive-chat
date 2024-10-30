8511e6e71f5cda4d114fa0288680785f5fb66c95
shRahat10 <shahriar.haque.1011@gmail.com>
2024-10-30 12:00:06 +0600

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,41 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//     origin: '*',
+//   });
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//     }),
+//   );
+
+//   await app.listen(process.env.PORT || 8082);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
 dotenv.config();
 
+const server = express();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   app.enableCors({
     origin: '*',
   });
@@ -17,7 +47,12 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  // app.setGlobalPrefix(process.env.GLOBAL_PREFIX || 'v1/api');
-  await app.listen(process.env.PORT || 8082);
+
+  await app.init();
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('NestJS application failed to start:', err);
+});
+
+export default server;
